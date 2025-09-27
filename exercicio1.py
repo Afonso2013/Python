a6be447c3eb7847a6ebdbd96d812efe067a283cf
Afonso2013 <shark08942@sharkcoders.pt>
2025-09-27 10:31:32 +0100

    Add files via upload
    operaoes matamaticas

---
+++ exercicio1.py
@@ -0,0 +1,14 @@
+n1 = 5
+n2 = 18
+n3 = 25
+n4 = 38
+n5 = 42
+n6 = 53
+soma = n4 + n3
+print("A soma dá:",soma)      
+subtrair = n2 - n1
+print("A subtração dá:",subtrair)
+multiplicar = n5 * n6
+print("a multiplicação dá:",multiplicar)
+dividir = n6 / n1
+print("A divisão dá:",dividir)
